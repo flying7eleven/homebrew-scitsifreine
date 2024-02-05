9e9e453ab6fff6c30d1d7fcd7111a9d0c82d03a1
Tim Janke <tim+github@janke.biz>
2024-02-05 08:19:23 +0100

    :bug: Add a HEAD option for the install

--- Formula/scitsifreine.rb
+++ Formula/scitsifreine.rb
@@ -3,11 +3,13 @@ class Scitsifreine < Formula
 
   desc "A small tool for using tmux for connecting to multiple remote servers via ssh."
   homepage ""
-  url "https://github.com/flying7eleven/scitsifreine/archive/refs/tags/0.1.0.tar.gz"
+  url "https://github.com/flying7eleven/scitsifreine.git",
+      tag:      "0.1.0",
+      revision: "0772966dfea9dd99c1bad97df0d2c0bfad5d11c5"
   sha256 "a789d6e8775a6dcd244d6f4b6f1353c0085d98403f924f067a52051a5cecdff0"
   license "MIT"
+  head "https://github.com/flying7eleven/scitsifreine.git", branch: "main"
 
-  depends_on "ansible"
   depends_on "python"
 
   def install
